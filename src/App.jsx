557dd5f3c6636f1b084f72733a7a440221628928
Damien <damienambient@gmail.com>
2023-09-12 22:51:32 +0300

    setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-    return <div>Hello World</div>;
+    return (
+        <>
+            <GlobalStyles></GlobalStyles>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replae to="dashboard"></Navigate>}
+                    ></Route>
+                    <Route
+                        path="dashboard"
+                        element={<Dashboard></Dashboard>}
+                    ></Route>
+                    <Route
+                        path="bookings"
+                        element={<Bookings></Bookings>}
+                    ></Route>
+                    <Route path="cabins" element={<Cabins></Cabins>}></Route>
+                    <Route path="users" element={<Users></Users>}></Route>
+                    <Route
+                        path="settings"
+                        element={<Settings></Settings>}
+                    ></Route>
+                    <Route path="account" element={<Account></Account>}></Route>
+                    <Route path="login" element={<Login></Login>}></Route>
+                    <Route
+                        path="#"
+                        element={<PageNotFound></PageNotFound>}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
