b2cc2487c6e8ef308822f564cebebde44b957659
Damien <damienambient@gmail.com>
2023-09-16 17:27:29 +0300

    supabase established and connected to react

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hzesenifmaqiekalacyk.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6ZXNlbmlmbWFxaWVrYWxhY3lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ4MTExNjMsImV4cCI6MjAxMDM4NzE2M30.7NosZNdoO7tW_iBIfxikxYXKR-7zjYag-aQxfmGtDuw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
