6e6c1a30f2557be9c708ad81dbc80909b2ec7624
Damien <damienambient@gmail.com>
2023-09-25 19:43:10 +0300

    render props pattern

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -87,7 +87,11 @@ function Row({ children }) {
         </StyledRow>
     );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+    if (!data.length) return <Empty>No data</Empty>;
+
+    return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Body = Body;
