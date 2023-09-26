9dc49a6f2c205af5e94288c3865619a2f6f5745b
Damien <damienambient@gmail.com>
2023-09-26 22:46:11 +0300

    reusable context menu

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Menus from "../../ui/Menus";
 
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
@@ -26,21 +27,25 @@ function CabinTable() {
     if (isLoading) return <Spinner />;
 
     return (
-        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-            <Table.Header>
-                <div></div>
-                <div>Cabin</div>
-                <div>Capacity</div>
-                <div>Price</div>
-                <div>Discount</div>
-                <div></div>
-            </Table.Header>
+        <Menus>
+            <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+                <Table.Header>
+                    <div></div>
+                    <div>Cabin</div>
+                    <div>Capacity</div>
+                    <div>Price</div>
+                    <div>Discount</div>
+                    <div></div>
+                </Table.Header>
 
-            <Table.Body
-                data={cabins}
-                render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-            ></Table.Body>
-        </Table>
+                <Table.Body
+                    data={cabins}
+                    render={(cabin) => (
+                        <CabinRow cabin={cabin} key={cabin.id} />
+                    )}
+                ></Table.Body>
+            </Table>
+        </Menus>
     );
 }
 
